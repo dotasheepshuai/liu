525a9f46ccbfba6d3d860cab790b407e5acbfafb
Tianyang Liu <>
2021-04-04 06:20:17 +0000

    lession 1

--- index.js
+++ index.js
@@ -1,1 +1,107 @@
-console.log('123');
+// var a = "xiaosiwole";
+// var b = "wow";
+// print(b);
+// print(a, 5);
+// console.log(b);
+// console.log(b);
+// console.log(b);
+// console.log(b);
+// b = "333"; // integration
+// console.log(b);
+// console.log(b);
+// print(b);
+// console.log(a);
+// console.log(a);
+// a = "xiao";
+// console.log(a);
+// var c = "cat";
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// console.log(c);
+// var d = ['haha', 'xixi', 'hehe', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// print(d[1]);
+// print(d);
+// console.log(d[11]);
+// print(d[13]);
+
+function print(something, count = 1) {
+  for (let i = 0; i < count; i++) {
+    console.log((new Date()).getDate() + ': ' +something);
+  }
+}
+
+// print(a);
+// print(a);
+// print(a);
+// print(a);
+// print(a);
+// print(a);
+// print(a);
+// print(a);
+// print(d, 2);
+// var p = "duck";
+// var animals = [p, "dog", "cat"];
+// var e = ["sheep", animals, p];
+// print(p);
+// print(animals[0]);
+// print(e[1][0], 2);
+
+// const apple = "pear"; // constant
+// print(apple, 3);
+
+// print("debug #1");
+// const a = "duck";
+// print(a);
+
+// print("debug #2"); const a = "duck"; print(a);
+
+//  print("debug #3");
+//  const a = "duck";
+//  print(a);
+
+// print("debug #4");
+// const a = "duck";
+// print(a);
+
+// print("debug #5");
+// const a = ["duck", "apple"];
+// print(a[0]);
+
+// print("debug #6");
+// const a = [["duck", "dog"], "apple"];
+// print(a[0][0]);
+
+// print("debug #7");
+// const a = [["pear", "dudu"], "duck"];
+// print(a[1]);
+
+// print("debug #8");
+// const a = [["pear", "duck"] "dog"];
+// print(a[0][1]);
+
+// print("debug #9");
+// const a = [["pear","duck"], "dog"];
+// print(a[0][1]);
+
+// print("debug #10");
+// const a = [["pear", "duck"], "dog"];
+// print(a[0][1]);
+
+// print("debug #11");
+// const a = [["pear", "duck"], "dog"];
+// print(a[0][1]);
+
+// print("debug #12");
+// const a = [["pear", "duck"], "dog"];
+// print(a[0][1]);
+
+const a = "123"; // 字符串
+const b = 123; //数字
+const c = ["1", "2", "3"]; //列表
+const d = { //对象
+  "hair": "black", 
+  "eye": "black"
+};
+print(d.hair);
