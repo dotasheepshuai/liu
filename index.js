c2ee7001107fa858dbc3eedf5f6abbf0a2e45bdc
huahua299 <>
2021-04-13 09:58:29 +0000

    美女有对象没？

--- index.js
+++ index.js
@@ -6,28 +6,7 @@ function print(something, count = 1) {
   }
 }
 
-
-
-// const SW = {
-//   socialWorkercha: ["huahua", "yangyang"],
-//   skillsOfIntervention: ["case", "groups", "community"],
-//   clients: [
-//     { name: "xiaofeiyang", age: 18 },
-//     { name: "xiaodu", age: 12 }
-//   ],
-//   problems: ["divorce", "bullying", "retirement"]
-// };
-// print(SW);
-// // print(SW.problems[random(0, 2)]);
-// // print(SW.clients[random(0, 1)]);
-// // print(SW.skillsOfIntervention[random(0, 2)]);
-// // print(SW.socialWorker[random(0, 1)]);
-// SW.resourse = ["policy", "volunteer", "fundation"];
-// // print(SW);
-
 function selectBeauty() {
-    
-
   return {
     age: getAge(), 
     height: getHeight(),
@@ -35,11 +14,12 @@ function selectBeauty() {
     cup: getCup(),
     gpa: getGpa(),
     name: getName(),
+    hasBoyfriend: getHasBoyfriend(),
   };
 }
 
 function getAge() {
-  return random(18, 30);
+  return random(16, 20);
 }
 
 function getHeight() {
@@ -51,30 +31,85 @@ function getWeight() {
 }
 
 function getCup() {
-  const cupOptions = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  const cupOptions = ['B', 'C', 'D', 'E'];
   const randomIndex = random(0, cupOptions.length - 1);
   return cupOptions[randomIndex];
 }
 
 function getGpa() {
-  return random(0, 100);
+  return random(20, 100);
 }
 
 // 需求：姓：赵钱孙李周吴郑王 名：lily,rose,flower,river,sheep,lion,hydrangea,cloud
 
 function getName() {
-  const fisrtName = ['lily', 'rose', 'flower', 'river', 'sheep', 'lion', 'hydrangea', 'cloud'];
-  const randomIndex1 = random(0, fisrtName.length - 1);
-  const lastName = [' zhao', ' qian', ' sun', ' lee', ' zhou', ' wu', ' zheng', ' wang'];
-  const randomIndex2 = random(0, lastName.length - 1);
+  const firstName = ['lily', 'rose', 'flower', 'river', 'sheep', 'lion', 'hydrangea', 'cloud', 'dudu', 'yangyang'];
+  const firstNameIndex = random(0, firstName.length - 1);
+  const lastName = ['zhao', 'qian', 'sun', 'lee', 'zhou', 'wu', 'zheng', 'wang'];
+  const lastNameIndex = random(0, lastName.length - 1);
   
-  return name = fisrtName[randomIndex1] + lastName[randomIndex2];
+  const name =firstName[firstNameIndex] + ' ' + lastName[lastNameIndex];
+  return name;
 
+}
+//维护性，清晰，明确//return出来一个结果
+
+
+// 1、50% has boyfriend
+// function getHasBoyfriend() {
+
+//   if (!!random(0,1)) {
+//     return('dating');
+
+//   } else {
+//     return('single');
+//   }
+// }
+
+//   const randomNumber = random(0, 100);
+//   const threshold = 30;
+// const a = randomNumber > threshold;
+
+
+function getHasBoyfriend() {
+
+  const randomNumber = random(1, 100);
+  const threshold = 30;
+  const a = randomNumber > threshold;
+
+  if (a) {
+    return("i have a boyfriend !");
+
+  } else {
+    return("i don't have a boyfriend!" );
+  }
 }
 
 
+/**
+ * homework1: 30% hasBoyfriend is true
+ * homework2: 40% hasBoyfriend is true
+ * homework3: if hasBoyfriend is true, print "i have a boyfriend !". if hasBoyfriend is false, print "i don't have a boyfriend!" 
+ * homework4: improve the code to make it as readible as possible.
+ */
+
+
+
+
+
+
+print('beauty1');
 const beauty1 = selectBeauty();
 print(beauty1);
 
+print('\n');
 
-print("tianyang是傻子")
+print('beauty2');
+const beauty2 = selectBeauty();
+print(beauty2);
+
+
+
+// const xiang = random(1.1, true);
+// print(xiang);
+
